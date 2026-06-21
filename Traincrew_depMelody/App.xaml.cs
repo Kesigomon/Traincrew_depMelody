76b6b06597baab748b3c294060bf299504bb4546
kesigomon <34025522+Kesigomon@users.noreply.github.com>
2026-06-21 18:37:45 +0900

    微妙なリファクタ

--- Traincrew_depMelody/App.xaml.cs
+++ Traincrew_depMelody/App.xaml.cs
@@ -73,7 +73,7 @@ public partial class App : System.Windows.Application
         // Presentation Layer
         services.AddSingleton<MainWindow>();
         services.AddTransient<SettingsWindow>();
-        services.AddSingleton<Func<SettingsWindow>>(sp => () => sp.GetRequiredService<SettingsWindow>());
+        services.AddSingleton<Func<SettingsWindow>>(sp => sp.GetRequiredService<SettingsWindow>);
 
         // Application Layer
         services.AddSingleton<IMelodyControlService, MelodyControlService>();
